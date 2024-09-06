2829abdb4a3f47479f8c40558ca2d32ff00c6044
Senagomesdev <Senagomes.dev@gmail.com>
2024-09-06 13:50:09 -0300

    Alteração de arquivos

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "A jovem skatista Rayssa Leal, conhecida como Fadinha, é um fenômeno do esporte. Com um estilo único e muita personalidade, ela conquistou o mundo com suas manobras incríveis. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8",
+    },
+
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Rodrigues de Andrade (Guarulhos, 8 de maio de 1999) é uma ginasta artística brasileira bicampeã olímpica e a maior medalhista da história do Brasil nas Olimpíadas, com 6 medalhas (2 ouros, 3 pratas e 1 bronze).",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "judo judoca judoka",
+    },
+
+    {
+        titulo: "Beatriz Souza",
+        descricao: "A judoca brasileira Beatriz Souza, conhecida por sua técnica impecável e determinação, conquistou o coração dos fãs do esporte. Com uma trajetória marcada por vitórias e superação, ela se destaca no cenário mundial do judô.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "ginástica ginastica ouro",
+    }
+
+]
